1e757e4790ada2717b3a37f7bbb38a033a03c47b
Vitor Hugo <vitor.hov@gmail.com>
2020-08-01 23:39:06 -0300

    Updates

---
+++ precache-manifest.f186f0778112308ce71cd000fa01323d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35801f79a98a10855159014a66202bf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6364e43a12cf386b1b8",
+    "url": "/static/js/2.31680cf8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.31680cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "642b95e39f9a6babb831",
+    "url": "/static/js/main.42873a88.chunk.js"
+  },
+  {
+    "revision": "8618a7eaaf24f3e4897d",
+    "url": "/static/js/runtime-main.8fa136a4.js"
+  }
+]);
